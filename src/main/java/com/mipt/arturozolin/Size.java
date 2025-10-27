ce73f0fe0a4e5bb75d64b955199084e9a630c2b8
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2025-10-27 12:42:22 +0300

    created Annotations

---
+++ src/main/java/com/mipt/arturozolin/Size.java
@@ -0,0 +1,14 @@
+package com.mipt.arturozolin;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Size {
+  int min() default 0;
+  int max() default 50;
+  String message() default "";
+}
