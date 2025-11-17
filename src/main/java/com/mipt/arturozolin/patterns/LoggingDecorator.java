d5e01cdbc133768dd48818567574f9c643be846a
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2025-11-17 10:27:02 +0300

    done hw

--- src/main/java/com/mipt/arturozolin/patterns/LoggingDecorator.java
+++ src/main/java/com/mipt/arturozolin/patterns/LoggingDecorator.java
@@ -11,16 +11,19 @@ public class LoggingDecorator implements DataService {
 
   @Override
   public Optional<String> findDataByKey(String key) {
+    System.out.println("Finding data by key: " + key);
     return wrappee.findDataByKey(key);
   }
 
   @Override
   public void saveData(String key, String data) {
+    System.out.println("Saving data with key: " + key);
     wrappee.saveData(key, data);
   }
 
   @Override
   public boolean deleteData(String key) {
+    System.out.println("Deleting data with key: " + key);
     return wrappee.deleteData(key);
   }
 }
