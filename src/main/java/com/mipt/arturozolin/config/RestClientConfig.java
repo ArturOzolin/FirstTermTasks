e4caed11e57a3cd9f103212e28a8b3c5efbedc7b
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2026-04-27 20:29:05 +0300

    feature: сделал RestClient к внешнему API

---
+++ src/main/java/com/mipt/arturozolin/config/RestClientConfig.java
@@ -0,0 +1,32 @@
+package com.mipt.arturozolin.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+@EnableConfigurationProperties(ExternalApiProperties.class)
+public class RestClientConfig {
+
+  @Bean(name = "externalRestClient")
+  public RestClient externalRestClient(ExternalApiProperties properties) {
+    HttpClient httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofMillis(properties.getConnectTimeoutMs()))
+            .build();
+
+    JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
+    requestFactory.setReadTimeout(Duration.ofMillis(properties.getReadTimeoutMs()));
+
+    return RestClient.builder()
+            .baseUrl(properties.getBaseUrl())
+            .defaultHeader(HttpHeaders.USER_AGENT, properties.getUserAgent())
+            .requestFactory(requestFactory)
+            .build();
+  }
+}
